37a5b65c39dc0657b3bb6c3a37ae2207062365af
Татьяна Хекало <hekalot@gmail.com>
2023-05-22 22:31:14 +0300

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.scss';
-import Header from './components/Header';
 import Home from './components/home/Home';
 
 
 const App = () => {
   return (
     <div className="App">
-      <Header />
       <Home />
     </div>
   );
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import './Home.scss';
+import Header from '../Header';
 
 const Home = () => {
   return (<>
     <div className='main_container'>
-      <div className='main_container-text'>
-        <h1 className='main_container-title'>WE PROMISE COMFORTABLE LIVING SPACES AND BEAUTIFUL DAYS TO LIVE.</h1>
-        <a href="#" className='button'><span className='button-text'>Scroll to discover</span></a>
-        <div className='main_container-social'>
-          <a href="#" className='social_link'>FACEBOOK</a>
-          <a href="#" className='social_link'>INSTAGRAM</a>
-          <a href="#" className='social_link'>YOUTUBE</a>
-        </div>
-      </div>
-      <div className='main_container-gallery'></div>
+      <Header />
     </div>
   </>)
 }
